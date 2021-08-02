b82f6e4400160ebe4d2c3abaaa9676d29f1cb8d8
Sidney Oluoch <xidstar@gmail.com>
2021-08-02 10:09:23 -0400

    amazon booklist

---
+++ src/books.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81QQltvT2sL._AC_UL200_SR200,200_.jpg',
+    title: 'Eyes That Kiss in the Corners',
+    author: 'Joanna Ho',
+    id: 1
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91eIYG5foQL._AC_UL200_SR200,200_.jpg',
+    title: 'Outlawed',
+    author: 'Anna North',
+    id: 2
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71HMEyo4RAL._AC_UL200_SR200,200_.jpg',
+    title: 'Black Buck',
+    author: 'Mateo Askaripour',
+    id: 3
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51GzewxBJrL._AC_UL200_SR200,200_.jpg',
+    title: 'Lore',
+    author: 'Alexandra Bracken',
+    id: 4
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91kLbd-Gn2L._AC_UL200_SR200,200_.jpg',
+    title: 'The Prophets',
+    author: 'Robert Jones Jr.',
+    id: 5
+  },
+]
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import css
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//import components
+import books from './books'
+import Book from './Book'
+
+function Booklist() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return (
+          <Book key={book.id}  {...book}></Book>
+        )
+      })}
+    </section>
+  )
+}
+
+ReactDOM.render(<Booklist />, document.getElementById('root'));
